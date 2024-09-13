30dc26799fb2c1422421536be4deae85eed6d2c8
Peder Espen <pederespen@consigli.no>
2024-09-14 01:12:54 +0200

    added logo and favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
   import Hourglass from './hourglass.svelte';
+  import LogoWithText from '../assets/juru-logo-with-text.svg'
 </script>
 
-<div class="relative flex items-center justify-center min-h-screen bg-egg-white text-gray-900 overflow-hidden">
-  <h1 class="text-5xl font-bold text-gray-500">Coming soon</h1>
+<div class="relative flex flex-col items-center justify-center min-h-screen bg-egg-white text-gray-900 overflow-hidden gap-y-6">
+  <img src={LogoWithText} alt="LogoWithText" class="mt-4 w-[200px]"/>
   <Hourglass color="#6b7280"/>
+  <h1 class="text-5xl font-bold text-gray-500">Coming soon</h1>
 </div>
