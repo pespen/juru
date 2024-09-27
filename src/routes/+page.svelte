1148d9296f617d08d3abedfd74f7f390c9d7f265
Peder Espen <pederespen@consigli.no>
2024-09-27 21:50:05 +0200

    added header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,3 @@
-<script>
-  import Hourglass from './hourglass.svelte';
-  import LogoWithText from '../assets/juru-logo-with-text.svg'
-</script>
-
-<div class="relative flex flex-col items-center justify-center min-h-screen text-light-6 overflow-hidden gap-y-6">
-  <img src={LogoWithText} alt="LogoWithText" class="mt-4 w-[200px]"/>
-  <Hourglass color="#6b7280"/>
-  <h1 class="text-5xl font-bold text-light-6">Coming soon</h1>
+<div class="">
+  <p>Juru</p>
 </div>
