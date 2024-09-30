b07e5c6a7017ba0e3385b63e142e3b0b605bdda0
Peder Espen <pederespen@consigli.no>
2024-09-30 09:16:40 +0200

    more text, more styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
 <div>
-  <h2 class="text-3xl font-bold text-light-6">Om oss</h2>
-  <p class="mt-4 text-light-6">Informasjon om PWE her. CV?</p>
+	<h2 class="text-3xl font-bold text-light-6">Om oss</h2>
+	<p class="mt-4 text-light-6">Informasjon om PWE her. CV?</p>
 </div>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,6 @@
 <div>
-  <h2 class="text-3xl font-bold text-light-6">Kontakt</h2>
-  <p class="mt-4 text-light-6">
-    Kontakt oss på e-post: test@epost.no eller telefon: 123 45 678.
-  </p>
+	<h2 class="text-3xl font-bold text-light-6">Kontakt</h2>
+	<p class="mt-4 text-light-6">
+		Kontakt oss på e-post: test@epost.no eller telefon: 123 45 678.
+	</p>
 </div>
