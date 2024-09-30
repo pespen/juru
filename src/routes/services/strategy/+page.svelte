5648078aa4d7d44674a8592f5935f39e3c614e45
Peder Espen <pederespen@consigli.no>
2024-09-30 11:02:46 +0200

    added text, missing legaladvice though

--- src/routes/services/strategy/+page.svelte
+++ src/routes/services/strategy/+page.svelte
@@ -1,4 +1,35 @@
 <div>
 	<h1 class="text-3xl font-bold mb-4">Strategisk juridisk rådgivning</h1>
-	<p class="mb-4">TEXT TEXT TEXT</p>
+	<p class="mb-4">
+		Hva er dette? Det handler om å se helhetlig på virksomheten;
+	</p>
+	<ol class="list-decimal list-inside mb-4">
+		<li>Hva har størst risiko forretningsmessig og juridisk?</li>
+		<li>Hva kan gi større lønnsomhet i et rettslig handlingsrom?</li>
+		<li>
+			Å spare penger ved å spisse oppdrag som må settes ut til advokater og
+			styre ressursbruken.
+		</li>
+	</ol>
+	<p class="mb-4">
+		En juridisk direktør er spesialist på disse forholdene og vil spare deg for
+		betydelige beløp til en oversiktlig og kontrollert kostnad!
+	</p>
+	<p class="mb-4">
+		Det handler om å se virksomheten under ett og både foreta en risikovurdering
+		av hva det er mest risiko knyttet til, er det konkurranserettslig,
+		avtalerettslig, personvernrettslig eller andre forhold det er knyttet store
+		konsekvenser av brudd? Eller har du fulgt med i nye krav til rapportering?
+	</p>
+	<p class="mb-4">
+		Men det kan også dreie seg om å utnytte muligheter i lovverket, hvor man
+		ofte kvier seg for å benytte eksterne advokater av hensyn til kostnadene som
+		fort løper løpsk med dagens timesatser?
+	</p>
+	<p class="mb-4">
+		Ikke minst er det behov for en ledelse å kunne kontrollere kostnadene ved å
+		benytte eksterne advokater. Vi har foretatt innkjøp av juridiske tjenester i
+		en årrekke og vant til å både forhandle gode priser, men også og ikke minst
+		å sikre at hvert enkelt oppdrag ikke omfatter mer enn strengt nødvendig.
+	</p>
 </div>
